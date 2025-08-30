7e638e323e7f8d2be9900e8da9dbd5a6c0e21681
jaspane <jaspane@gmail.com>
2025-08-30 11:27:50 -0700

    Fix JSX Syntax Errors in ProposalGenerator

--- src/components/ProposalGenerator.tsx
+++ src/components/ProposalGenerator.tsx
@@ -1021,7 +1021,6 @@ const ProposalGenerator: React.FC = () => {
                             </div>
                           ))}
                         </div>
-                      </div>
                     ))}
                   </div>
                 </div>
